e935511496a42189e72d6d65610b8e9a635e0985
sandip satyal <sandipsatyal25@gmail.com>
2020-10-02 17:23:23 +0545

    [add] markup for header section

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "./sections/Navbar/index";
+import Header from "./sections/Header";
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
     </div>
   );
 }
---
+++ src/sections/Header/index.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./header.css";
+import { AiFillInstagram, AiOutlineTwitter } from "react-icons/ai";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="header__content">
+        <h1>Namaste I'm Ishan Subedi</h1>
+        <h5 className="text-center">Kickass Vue/Vuetify and Nuxt Rockstar</h5>
+        <h5 className="text-center">from the roots of himalayas.</h5>
+        <button className="header__button">About me</button>
+      </div>
+      <div className="header__social-media">
+        <ul className="header__social-media--list">
+          <li className="header__social-media--list--item">
+            <AiOutlineTwitter />
+          </li>
+          <li className="header__social-media--list--item">
+            <AiFillInstagram />
+          </li>
+        </ul>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
