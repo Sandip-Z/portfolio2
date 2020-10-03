361589d61f9e4fa4d1c7ed4fe51a36ae5fddd075
sandip satyal <sandipsatyal25@gmail.com>
2020-10-03 14:06:14 +0545

    [minor layout fixes

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { IoMdArrowForward } from "react-icons/io";
 
-const ProjectContainer = ({ project }) => {
+const ProjectContainer = ({ project, index }) => {
   const url = `../assets/${project.background}`;
   return (
     <div
       className="d-flex flex-column project-container"
-      style={{ backgroundImage: `url(${url})` }}
+      style={{
+        backgroundImage: `url(${url})`,
+        height: index % 3 === 0 || index % 4 === 0 ? "200px" : "400px",
+      }}
     >
       <h2>{project.name}</h2>
       <p>{project.technology}</p>
--- src/sections/Work/index.js
+++ src/sections/Work/index.js
@@ -5,8 +5,8 @@ import ProjectContainer from "../../components/ProjectContainer";
 import SectionHeading from "../../components/SectionHeading";
 
 const Work = () => {
-  const renderProjects = projects.map((project) => {
-    return <ProjectContainer project={project} />;
+  const renderProjects = projects.map((project, index) => {
+    return <ProjectContainer project={project} index={index + 1} />;
   });
   return (
     <section className="section__wrapper work">
@@ -15,7 +15,7 @@ const Work = () => {
         description=" Lorem Ipsum is a text which is very random in nature and for this
           section we dont need it to be long"
       />
-      <div className="d-flex flex-wrap">{renderProjects}</div>
+      <div className="d-flex projects">{renderProjects}</div>
     </section>
   );
 };
