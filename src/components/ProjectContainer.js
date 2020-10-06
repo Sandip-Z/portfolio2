481c9879a489c0adeb64127ff033d352b856fd32
sandip satyal <sandipsatyal25@gmail.com>
2020-10-06 15:45:42 +0545

    [add] linear-gradient on project hover

--- src/components/ProjectContainer.js
+++ src/components/ProjectContainer.js
@@ -1,18 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import { IoMdArrowForward } from "react-icons/io";
 
 const ProjectContainer = ({ project, index }) => {
-  const url = project.background
-    ? require(`../assets/${project.background}`)
-    : "";
+  const [url] = useState(
+    project.background ? require(`../assets/${project.background}`) : ""
+  );
+
+  const [gradient, setGradient] = useState("");
+
   return (
     <div
       className="d-flex flex-column project-container"
       style={{
-        backgroundImage: `url(${url})`,
+        backgroundImage: `${gradient}${gradient ? "," : ""} url(${url})`,
         backgroundSize: "cover",
         height: index % 3 === 0 || index % 4 === 0 ? "200px" : "400px",
       }}
+      onMouseOver={() =>
+        setGradient("linear-gradient(to right, #666a6a87, transparent)")
+      }
+      onMouseLeave={() => setGradient("")}
     >
       <h2>{project.name}</h2>
       <p>{project.technology}</p>
